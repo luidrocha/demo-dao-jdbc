e5cadb268158feb5497d8ec3318e36e7ae351d36
LuidRocha <luidrocha@yahoo.com.br>
2020-02-11 22:05:13 -0300

    Seller Entity class

--- src/Aplication/Program.java
+++ src/Aplication/Program.java
@@ -1,16 +1,26 @@
 package Aplication;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main (String[] args) {
 		
 		
+		
 		Department obj = new Department(1,"Books");
 		
 		System.out.println(obj);
 		
+		Seller sl  =new Seller(1, "Jose","luidrocha@yahoo.com.br", new Date(),obj, 3000.0 );
+		
+		System.out.println(sl);
+		
 	}
-	
+
 }
